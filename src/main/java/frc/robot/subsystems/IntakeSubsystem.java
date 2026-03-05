ba56beb82f2f2625233238fac79a7785e5847e67
Mulgrave Robotics 9649 <mulgraverobotics@gmail.com>
2026-03-05 08:51:22 -0800

    uunfinished in progress commit

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,25 +4,83 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
 
 public class IntakeSubsystem extends SubsystemBase {
     private final SparkMax m_intakeMotor;
+    private final SparkMax m_intakePivMotor;
+    private final RelativeEncoder m_intakePivEncoder;
+    public double CurState;
 
     public IntakeSubsystem() {
         m_intakeMotor = new SparkMax(IntakeConstants.IntakeMotorCanId, MotorType.kBrushless);
+        m_intakePivMotor = new SparkMax(IntakeConstants.IntakePivMotorCanID, MotorType.kBrushless);
+
+        m_intakePivEncoder = m_intakePivMotor.getEncoder();
 
         SparkMaxConfig config = new SparkMaxConfig();
         config.smartCurrentLimit(40).idleMode(IdleMode.kBrake);
+
         m_intakeMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        m_intakePivMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+        
+        
+    }
+
+    public double getPivPos(){
+        return Math.abs(m_intakePivEncoder.getPosition());
+    }
+
+    public void setPivotState (double tarState){
+        CurState = getPivPos();
+        double speed = 0.0;
+
+        if (tarState > CurState){
+            speed = IntakeConstants.RollerSpeed*-1;
+        }
+
+        else if (tarState < CurState){
+            speed = IntakeConstants.RollerSpeed;
+        }
+
+        else{
+            
+        }
+
+        m_intakeMotor.set(speed);
+        SmartDashboard.putNumber("Current Intake Pivot State", CurState);
+
     }
 
     public Command runIntakeCommand(double speed) {
         return run(() -> m_intakeMotor.set(speed));
+    }
+
+    public Command setPivotCommand(double tarState){
+        return run(() -> setPivotState(tarState));
+    }
+
+    public Command MoveTo(double tarState){
+        if (tarState = 0.0){
+            return setPivotCommand(tarState).until(() -> aroundState(tarState)).andThen(() -> (m_intakePivMotor.set(0)));
+        }
+    }
+
+    public boolean aroundState(double state){
+        return aroundState(state, IntakeConstants.tol);
+    }
+
+    public boolean aroundState(double state, double tol){
+        return MathUtil.isNear(state, getPivPos(), tol);
     }
 
     public void stop() {
